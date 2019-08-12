c6e76a5e8994f492cb1beba65279ed175d91d1f1
klntsky <klntsky@gmail.com>
2019-08-12 18:03:35 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# npm-run-graph
+
+Sometimes using task runners isn't desired, and we end up adding a lot of fields to the `scripts` section of a `package.json`.
+
+This tiny (<50 LOC) script helps to visualize the build process by drawing a graph showing how `npm run` commands depend on each other.
+
+An example of such a graph for a real-world project:
+
+![npm-run-graph.png](npm-run-graph.png)
+
+[Graphviz](https://graphviz.org/) is a runtime dependency.
---
+++ index.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+/* global require */
+const graphviz = require('graphviz'),
+      fs = require('fs'),
+      process = require('process'),
+      color = require('rcolor');
+
+const package = JSON.parse(fs.readFileSync('./package.json'));
+
+if (typeof package.scripts !== 'object') {
+  console.log('`package.json` does not contain a field named `scripts`.');
+  process.exit(1);
+}
+
+const graph = graphviz.digraph(package.name || 'unknown');
+const colorMap = new Map();
+const re = /npm\s+run\s+([a-zA-Z-_:]+)/g;
+
+Object.keys(package.scripts).forEach(script => {
+  colorMap.set(script, color());
+});
+
+Object.entries(package.scripts).forEach(([key, value])  => {
+  var match, matches = [];
+
+  graph.addNode(key, { color: colorMap.get(key), penwidth: 2 });
+
+  while ((match = re.exec(value))) {
+    matches.push(match[1]);
+  }
+
+  matches.forEach((match, ix) => {
+    const edge = graph.addEdge(key, match, {
+      penwidth: 2,
+      color: colorMap.get(key),
+      fontcolor: colorMap.get(key)
+    });
+
+    if (matches.length > 1)
+      edge.set('label', ix + 1);
+  });
+});
+
+console.log(graph.to_dot());
+
+graph.output("png", "./npm-run-graph.png");
